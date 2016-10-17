c3fca3d48147d9f7ff8c533d9d1f8b88887b87fa
mkg20001 <mkg20001@gmail.com>
2016-10-17 16:52:25 +0200

    Make Button in AboutActivity white

--- mobile/src/main/java/mkg20001/net/samremote/AboutActivity.java
+++ mobile/src/main/java/mkg20001/net/samremote/AboutActivity.java
@@ -9,13 +9,14 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
+import mkg20001.net.samremotecommon.Tools;
+
 public class AboutActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
 
-        //Intent intent = getIntent();
         FloatingActionButton fab=(FloatingActionButton) findViewById(R.id.stateFAB);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -25,18 +26,12 @@ public class AboutActivity extends AppCompatActivity {
             }
         });
 
-        Drawable draw= ContextCompat.getDrawable(AboutActivity.this,R.drawable.close);
+        Drawable draw = ContextCompat.getDrawable(AboutActivity.this,R.drawable.close);
+        draw.setColorFilter(Tools.filter());
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             fab.setForeground(draw);
         } else {
             fab.setImageDrawable(draw);
         }
-        /*String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
-        TextView textView = new TextView(this);
-        textView.setTextSize(40);
-        textView.setText(message);
-
-        ViewGroup layout = (ViewGroup) findViewById(R.id.activity_display_message);
-        layout.addView(textView);*/
     }
 }
